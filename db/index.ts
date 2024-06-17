a15905d712c34375ecb4b9f04f162862eaa3a6d5
shahzad67432 <shaa1891640@gmail.com>
2024-06-18 03:25:31 +0500

    next auth handker

--- db/index.ts
+++ db/index.ts
@@ -1,15 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
 
-const PrismaClientSingleton = ()=>{
-    return new PrismaClient()
-}
+// const PrismaClientSingleton = ()=>{
+//     return new PrismaClient()
+// }
 
-declare global {
-    var prismaGlobal: undefined | ReturnType<typeof PrismaClientSingleton>
-}
+// declare global {
+//     var prismaGlobal: undefined | ReturnType<typeof PrismaClientSingleton>
+// }
 
-const prisma: ReturnType<typeof PrismaClientSingleton> = globalThis.prismaGlobal ?? PrismaClientSingleton();
+// const prisma: ReturnType<typeof PrismaClientSingleton> = globalThis.prismaGlobal ?? PrismaClientSingleton();
+const prisma = new PrismaClient();
 export default prisma;
 
-if(process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
+// if(process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
